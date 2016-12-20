8ef881c2646fa6b55e6f4b1b1fc050bf383f46ef
estermer <eric.stermer@gmail.com>
2016-12-20 16:02:18 -0500

    dded comments to practice the syntax for comments in the code

---
+++ comments.java
@@ -0,0 +1,10 @@
+public class comments {
+  public static void main(String[] args) {
+    //this is a comment for a single line
+    /*
+    this is a multi line comment
+    */
+    // cmd+/ will comment the line that you are currently on in atom
+    // this is the same type of commenting syntax you find in JavaScript
+  }
+}
